60810dcbb3630cc1c55aaea669afcd7721a7abaa
Mahe Grandisson <mahe.grandisson@univ-tlse3.fr>
2024-11-20 15:30:52 +0100

    test_taskk added

---
+++ test_task.py
@@ -0,0 +1,14 @@
+import numpy as np
+from task import Task
+import unittest
+
+
+class TestAXIsB(unittest.TestCase):
+    def test(self):
+        task = Task()
+        task.work()
+        np.testing.assert_allclose(np.dot(task.a, task.x), task.b, rtol=1e-5, atol=1e-8)
+
+
+if __name__ == "__main__":
+    unittest.main()
